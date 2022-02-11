2bfae4ea8c4f002248a8c65d0073e78f723b1f5c
miyazaki8456 <96578528+miyazaki8456@users.noreply.github.com>
2022-02-12 07:27:59 +0900

    Add files via upload

---
+++ view.py
@@ -0,0 +1,26 @@
+from os import system
+import eel
+import desktop
+import task2
+
+app_name="html"
+end_point="index.html"
+size=(700,780)
+
+@ eel.expose
+def main(search_keyword, is_option: bool = False, page_limit: int=1, hidden_chrome: bool=False):
+    task2.main(search_keyword)
+    
+    return True
+@ eel.expose
+def clear():
+    
+    eel.view_log_js("")
+
+
+
+
+
+
+
+desktop.start(app_name,end_point,size)
